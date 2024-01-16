f12da54b3dc3edccb4c5a11ae57761efb4824ebe
Pietro Malzone <145590252+Pit17@users.noreply.github.com>
2024-01-16 13:40:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // (YYYY, M-1, D, H, M, S)
+    const countdownDate = new Date(2024, 5, 7, 12, 0, 0).getTime();
+
+    const countdownElement = document.getElementById('countdown');
+
+    
+    const countdownInterval = setInterval(function () {
+        const now = new Date().getTime();
+        const distance = countdownDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        countdownElement.innerHTML = `${days}-Giorni ${hours}-Ore ${minutes}-Minuti ${seconds}-Secondi`;
+
+        
+        if (distance < 0) {
+            clearInterval(countdownInterval);
+            countdownElement.innerHTML = 'Ci vediamo a Settembre!';
+        }
+    }, 1000);
+});
